bce6398a1e5a1ce9dec123eb2017c21497059f2f
Satish Babu <sierra1bravo@gmail.com>
2020-09-10 12:11:24 +0530

    First build.

--- DigiFiatSim.java
+++ DigiFiatSim.java
@@ -113,20 +113,7 @@ class MainWindow extends JFrame implements ActionListener {  //creation of an wi
   }
 }
 
-class PSP2 extends TimerTask {
-  MainWindow m;
 
-  public PSP2(MainWindow m1) {
-    this.m = m1;
-  }
-  public void run() {
-    try {
-      m.PSP2Label1.setText(""+(int)(Math.random() * 5000 + 1));
-      m.PSP2Label2.setText(""+(int)(Math.random() * 50000 + 1));
-      //Thread.sleep(500);
-    } catch(Exception e) {}
-  }
-}
 
 public class DigiFiatSim {
   static public void main(String[] args) {
